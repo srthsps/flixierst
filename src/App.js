1c6cb9ffa8f888ef36854e02f6c28e9fe2be7ed0
Sarath Sps <srthsps22@gmail.com>
2021-06-15 00:26:52 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Row from './components/Row'
 import requests from './utils/requests'
 import './App.css'
 import Banner from './components/Banner'
+import Nav from './components/Nav'
 
 const App=()=> {  
   const [data,setData] = useState([])
 
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRaw />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
---
+++ src/components/Nav.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import './Nav.css'
+import classnames from 'classnames'
+
+const Nav = () => {
+
+    const [show, handleShow] = useState(false)
+
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true)
+            }
+            else {
+                handleShow(false)
+            }
+        })
+        return () => {
+            window.removeEventListener("scroll")
+        }
+    },[])
+
+    return (
+        <div className={`nav ${show && "nav_black"}`}>
+            <img className="nav_logo"
+                src="netflix-logo.png"
+                alt="Netflix Logo" />
+            <img className="smiley_logo"
+                src="smiley_logo.jpg"
+                alt="Netflix Logo" />
+        </div>
+    )
+}
+
+export default Nav
